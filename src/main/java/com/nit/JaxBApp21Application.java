774aa2501f40112b30448f33772bf3c2eeb59004
gsrinupatra <gsrinupatra780@gmail.com>
2023-11-08 14:09:03 +0530

    first commit

---
+++ src/main/java/com/nit/JaxBApp21Application.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaxBApp21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaxBApp21Application.class, args);
+	}
+
+}
